610955d17443a703fd39d94819225b484f4525be
CoYe <wiseman.kostya@gmail.com>
2016-04-22 15:23:52 +0300

    Fix a logical issue with defining inject snmp params

--- cloudshell/configuration/cloudshell_snmp_configuration.py
+++ cloudshell/configuration/cloudshell_snmp_configuration.py
@@ -1,17 +1,20 @@
 from cloudshell.snmp.quali_snmp import QualiSnmp
 from cloudshell.shell.core.context.context_utils import get_attribute_by_name_wrapper, get_resource_address
 
-kwargs = {'ip': get_resource_address,
-          'snmp_version': get_attribute_by_name_wrapper('SNMP Version'),
-          'snmp_user': get_attribute_by_name_wrapper('SNMP User'),
-          'snmp_password': get_attribute_by_name_wrapper('SNMP Password'),
-          'snmp_community': get_attribute_by_name_wrapper('SNMP Read Community'),
-          'snmp_private_key': get_attribute_by_name_wrapper('SNMP Private Key')}
+QUALISNMP_INIT_PARAMS = {'ip': get_resource_address,
+                         'snmp_version': get_attribute_by_name_wrapper('SNMP Version'),
+                         'snmp_user': get_attribute_by_name_wrapper('SNMP User'),
+                         'snmp_password': get_attribute_by_name_wrapper('SNMP Password'),
+                         'snmp_community': get_attribute_by_name_wrapper('SNMP Read Community'),
+                         'snmp_private_key': get_attribute_by_name_wrapper('SNMP Private Key')}
+
 
 def create_snmp_handler():
-    for key, value in kwargs.iteritems():
+    kwargs = {}
+    for key, value in QUALISNMP_INIT_PARAMS.iteritems():
         if callable(value):
             kwargs[key] = value()
     return QualiSnmp(**kwargs)
+
 
 SNMP_HANDLER = create_snmp_handler
