5d52ff26994167a5c55260d7be4e955340a54c78
Constantine Yevdyukhin <Costya-Y@users.noreply.github.com>
2018-11-09 18:54:53 +0200

    added few more tests

--- tests/snmp/quali_snmp/tests_quali_snmp_methods.py
+++ tests/snmp/quali_snmp/tests_quali_snmp_methods.py
@@ -3,7 +3,7 @@ from mock import MagicMock, patch
 
 import cloudshell.snmp.quali_snmp as quali_snmp
 
-from cloudshell.snmp.snmp_parameters import SNMPV3Parameters
+from cloudshell.snmp.snmp_parameters import SNMPV3Parameters, SNMPV2ReadParameters
 
 
 def return_community(community):
@@ -11,6 +11,10 @@ def return_community(community):
 
 
 class TestQualiSnmpInit(TestCase):
+    SNMP_V3_PARAMS = SNMPV3Parameters(ip="localhost", snmp_user="user", snmp_password="pass",
+                                      snmp_private_key="priv_key")
+    SNMP_V2_PARAMS = SNMPV2ReadParameters(ip="localhost", snmp_read_community="test")
+
     @patch("cloudshell.snmp.quali_snmp.view")
     @patch("cloudshell.snmp.quali_snmp.cmdgen")
     @patch("cloudshell.snmp.quali_snmp.QualiSnmp.initialize_snmp")
@@ -25,9 +29,7 @@ class TestQualiSnmpInit(TestCase):
 
         view_mock.MibViewController().getNodeLocation.return_value = ("SNMPv2-MIB", "sysDescr", "0")
 
-        snmp_v3_params = SNMPV3Parameters(ip="localhost", snmp_user="user", snmp_password="pass",
-                                          snmp_private_key="priv_key")
-        return quali_snmp.QualiSnmp(snmp_parameters=snmp_v3_params, logger=self._logger)
+        return quali_snmp.QualiSnmp(snmp_parameters=self.SNMP_V3_PARAMS, logger=self._logger)
 
     @patch("cloudshell.snmp.quali_snmp.view")
     @patch("cloudshell.snmp.quali_snmp.cmdgen")
@@ -37,24 +39,69 @@ class TestQualiSnmpInit(TestCase):
         result.prettyPrint.return_value = "response"
         cmdgen_mock.CommandGenerator().getCmd.return_value = "", "", "", [["view", result]]
 
-        snmp_v3_params = SNMPV3Parameters(ip="localhost", snmp_user="user", snmp_password="pass",
-                                          snmp_private_key="priv_key")
-        quali_snmp.QualiSnmp(snmp_parameters=snmp_v3_params, logger=MagicMock())
+        quali_snmp.QualiSnmp(snmp_parameters=self.SNMP_V3_PARAMS, logger=MagicMock())
 
-        init_mock.assert_called_once_with(snmp_v3_params)
+        init_mock.assert_called_once_with(self.SNMP_V3_PARAMS)
 
-    @patch("cloudshell.snmp.quali_snmp.ObjectIdentity")
-    @patch("cloudshell.snmp.quali_snmp.QualiSnmp._command")
-    def test_get(self, cmd_mock, obj_id_mock):
+    @patch("cloudshell.snmp.quali_snmp.view")
+    @patch("cloudshell.snmp.quali_snmp.cmdgen")
+    @patch("cloudshell.snmp.quali_snmp.UsmUserData")
+    @patch("cloudshell.snmp.quali_snmp.QualiSnmp._test_snmp_agent")
+    def test_snmp_initialize_v3(self, test_mock, usm_mock, cmdgen_mock, view_mock):
         # Setup
-        quali_snmp = self.set_up()
+        view_mock.MibViewController().getNodeLocation.return_value = ("SNMPv2-MIB", "sysDescr", "0")
 
         # Act
-        result = quali_snmp.get("1.2.3.4")
+        quali_snmp.QualiSnmp(snmp_parameters=self.SNMP_V3_PARAMS, logger=MagicMock())
 
         # Assert
-        self.assertIsNotNone(result)
-        cmd_mock.assert_called_once_with(self._cmdgen_mock.CommandGenerator().getCmd, obj_id_mock.return_value)
+        test_mock.assert_called_once()
+        cmdgen_mock.UdpTransportTarget.assert_called_once_with((self.SNMP_V3_PARAMS.ip, self.SNMP_V3_PARAMS.port))
+        usm_mock.assert_called_once_with(userName=self.SNMP_V3_PARAMS.snmp_user,
+                                         authKey=self.SNMP_V3_PARAMS.snmp_password,
+                                         privKey=self.SNMP_V3_PARAMS.snmp_private_key,
+                                         authProtocol=self.SNMP_V3_PARAMS.auth_protocol,
+                                         privProtocol=self.SNMP_V3_PARAMS.private_key_protocol)
+
+    @patch("cloudshell.snmp.quali_snmp.view")
+    @patch("cloudshell.snmp.quali_snmp.cmdgen")
+    @patch("cloudshell.snmp.quali_snmp.QualiSnmp._test_snmp_agent")
+    def test_snmp_initialize_v2(self, test_mock, cmdgen_mock, view_mock):
+        # Setup
+        view_mock.MibViewController().getNodeLocation.return_value = ("SNMPv2-MIB", "sysDescr", "0")
+
+        # Act
+        quali_snmp.QualiSnmp(snmp_parameters=self.SNMP_V2_PARAMS, logger=MagicMock())
+
+        # Assert
+        test_mock.assert_called_once()
+        cmdgen_mock.UdpTransportTarget.assert_called_once_with((self.SNMP_V2_PARAMS.ip, self.SNMP_V2_PARAMS.port))
+        cmdgen_mock.CommunityData.assert_called_once_with(self.SNMP_V2_PARAMS.snmp_community)
+
+    @patch("cloudshell.snmp.quali_snmp.QualiSnmp.get")
+    def test_snmp_agent_test(self, get_mock):
+        # Setup
+        quali_snmp = self.set_up()
+        expected_call_params = ('SNMPv2-MIB', 'sysObjectID', '0')
+
+        # Act
+        quali_snmp._test_snmp_agent()
+
+        # Assert
+        get_mock.assert_called_once_with(expected_call_params)
+
+    @patch("cloudshell.snmp.quali_snmp.ObjectIdentity")
+    def test_get(self, obj_id_mock):
+        # Setup
+        quali_snmp = self.set_up()
+        oid = MagicMock()
+
+        # Act
+        quali_snmp._command(self._cmdgen_mock.CommandGenerator().getCmd, oid)
+
+        # Assert
+        self.assertIsNotNone(quali_snmp.var_binds)
+        self._cmdgen_mock.CommandGenerator().getCmd.assert_called_once_with(quali_snmp.security, quali_snmp.target, oid)
 
     @patch("cloudshell.snmp.quali_snmp.QualiSnmp.get")
     def test_get_property(self, get_mock):
@@ -115,3 +162,16 @@ class TestQualiSnmpInit(TestCase):
 
         # Assert
         cmd_mock.assert_called_once_with(self._cmdgen_mock.CommandGenerator().setCmd, obj_type_mock.return_value)
+
+    def test_command(self, get_mock):
+        # Setup
+        quali_snmp = self.set_up()
+        mib = "SNMPv2-MIB"
+        mib_property = "sysDescr"
+        index = "1"
+
+        # Act
+        result = quali_snmp._command(mib, mib_property, index)
+
+        # Assert
+        self.assertIsNotNone(result)
