27238c46eb2b9681f059504ff21ba4667761de66
Costya <costya.y@qualisystems.com>
2022-09-21 18:33:12 +0300

    added fix for ipv6

--- tests/snmp/core/tools/test_snmp_transport.py
+++ tests/snmp/core/tools/test_snmp_transport.py
@@ -30,6 +30,7 @@ class TestSnmpTransport(unittest.TestCase):
 
     def test_snmp_transport_with_hostname(self):
         hostname = "localhost"
+        ipv6 = "::1"
         ip = "127.0.0.1"
         snmp_community = "public"
         snmp_parameters = SNMPReadParameters(ip=hostname, snmp_community=snmp_community)
@@ -38,13 +39,13 @@ class TestSnmpTransport(unittest.TestCase):
         snmp_data = getTargetAddr(self._engine, "tgt")
         snmp_ip = snmp_data[1][0]
         snmp_port = snmp_data[1][1]
-        assert snmp_ip == ip
+        assert snmp_ip == ip or snmp_ip == ipv6
         assert snmp_port == snmp_parameters.port
 
     def test_snmp_transport_with_ipv6(self):
-        ip = "::1"
+        ipv6 = "::1"
         snmp_community = "public"
-        snmp_parameters = SNMPReadParameters(ip=ip, snmp_community=snmp_community)
+        snmp_parameters = SNMPReadParameters(ip=ipv6, snmp_community=snmp_community)
         transport = SnmpTransport(snmp_parameters, Mock())
         transport.add_udp_endpoint(self._engine, 10, 10)
         snmp_data = getTargetAddr(self._engine, "tgt")
